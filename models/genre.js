31e469b90bfa1ff931f28b5fd7db57977c7d1601
JacobAlex <dzhanaliyev@mail.ru>
2016-11-14 23:00:26 +0600

    Create genre.js

---
+++ models/genre.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+var genreSchema = new mongoose.Schema({
+    
+        name: String,
+		
+		author: {
+			id: 
+				{
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "User"
+		    },
+	     	username: String
+	    }
+});
+
+module.exports = mongoose.model("Genre", genreSchema);
