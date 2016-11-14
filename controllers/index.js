ef43fa4e55bd8ca2ea8c236ccf7a568910a030a8
JacobAlex <dzhanaliyev@mail.ru>
2016-11-14 23:01:52 +0600

    Create index.js

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+// Setup
+var express = require('express');
+var router = express.Router();
+
+// Home Page
+router.get('/', function(req, res, next) {
+  	res.render('index');
+});
+
+module.exports = router;
